6adeb480510303414fc1ce08afa31cdf2ae72d01
PoWerGooS <77084057+PoWerGooS@users.noreply.github.com>
2022-02-12 12:41:57 -0500

    Add files via upload

---
+++ guessingGame.py
@@ -0,0 +1,12 @@
+from random import random
+
+
+f = random(1-9)
+
+while chances < 5:
+    
+if guess === number:
+    print("Congratulations you won")
+    break
+if not chances < 5:
+    print("You lose the number is", number)
